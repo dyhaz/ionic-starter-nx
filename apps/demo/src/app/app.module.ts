67b0db49e5be1c2e8b951d0facb0f73d1f98a45b
dyhaz <muhammad-11@fst.unair.ac.id>
2021-10-04 08:56:24 +0700

    add ngxs store package

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -6,11 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    NgxsModule.forRoot([ZooState], {
+      developmentMode: !environment.production
+    })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
